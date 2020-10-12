3b93bb799b84080747c98c6c5124bac80f908bad
Marc Görtz <mail@marcgoertz.de>
2020-10-12 14:13:36 +0200

    Update build process to use a JSON file as source.

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,30 @@
 const gulp = require('gulp');
+const clean = require('gulp-clean');
+const copy = require('gulp-copy');
+const replace = require('gulp-string-replace');
 const {zip} = require('gulp-vinyl-zip');
 
-gulp.task('default', () =>
-	gulp.src('src/**/*')
+gulp.task('cleanup', () => gulp.src('./build', {read: false, allowEmpty: true})
+	.pipe(clean())
+);
+
+gulp.task('copy', () => gulp.src('./src/**/*')
+	.pipe(copy('./build', {prefix: 1}))
+);
+
+gulp.task('build', gulp.series('cleanup', 'copy', () => gulp.src('./build/info.plist')
+	.pipe(replace('%status-codes%', () => {
+		// eslint-disable-next-line import/no-unresolved
+		const data = require('./build/status-codes.json');
+		return JSON.stringify(data);
+	}))
+	.pipe(gulp.dest('build'))
+));
+
+gulp.task('zip', () =>
+	gulp.src('./build/**/*')
 		.pipe(zip('http-status.alfredworkflow'))
 		.pipe(gulp.dest('dist'))
 );
+
+gulp.task('default', gulp.series('build', 'zip', 'cleanup'));
